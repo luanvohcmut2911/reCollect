3b3807e8296afe83f7bc3e7c3756ad53014d5c94
luanvohcmut2911 <luan.vo2003@hcmut.edu.vn>
2023-03-11 23:55:55 +0700

    update sign in page

--- src/Pages/Signin.js
+++ src/Pages/Signin.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Button, Checkbox, Form, Input, Typography } from "antd";
 import styled from "styled-components";
-import { UserOutlined, GoogleOutlined, LockOutlined } from "@ant-design/icons";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import GoogleIcon from "../icons/GoogleIcon";
 
 const WrapperStyled = styled.div`
   margin: 0;
@@ -19,7 +20,7 @@ const InputStyled = styled(Input)`
   padding-top: 1rem;
   padding-bottom: 1rem;
   border-radius: 40px;
-`
+`;
 
 const onFinish = (values) => {
   console.log("Success:", values);
@@ -33,20 +34,21 @@ const Signin = () => {
       <Form
         name="normal-login"
         style={{
-          width: '500px',
+          width: "500px",
           backgroundColor: "white",
-          padding: "5rem",
+          padding: "0rem 2.5rem 5rem 2.5rem",
           borderRadius: "24px",
-
         }}
+        layout="vertical"
         initialValues={{ remember: true }}
         onFinish={onFinish}
       >
-        <Typography.Title>Sign In</Typography.Title>
+        <Typography.Title>Sign in</Typography.Title>
         <Typography.Text>
           New user? <a href="/signup">Create new account</a>
         </Typography.Text>
         <Form.Item
+          label={<Typography.Text strong>Username:</Typography.Text>}
           name="username"
           rules={[{ required: true, message: "Please enter your username! " }]}
         >
@@ -56,8 +58,9 @@ const Signin = () => {
           />
         </Form.Item>
         <Form.Item
+          label={<Typography.Text strong>Password:</Typography.Text>}
           name="password"
-          rules={[{ required: true, message: "Please input your Password!" }]}
+          rules={[{ required: true, message: "Please input your password!" }]}
         >
           <InputStyled
             prefix={<LockOutlined className="site-form-item-icon" />}
@@ -70,18 +73,81 @@ const Signin = () => {
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
 
-          <a className="login-form-forgot" href="123">
-            Forgot password
+          <a className="login-form-forgot" href="/" style={{
+            float: 'right'
+          }} >
+            Forgot your password?
           </a>
         </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
-            Log in
+        <Form.Item
+          style={{
+            position: "relative",
+          }}
+        >
+          <Button
+            type="primary"
+            htmlType="submit"
+            style={{
+              borderRadius: "50px",
+              padding: "24px",
+              display: "flex",
+              alignItems: "center",
+              float: "right",
+            }}
+          >
+            <Typography.Text
+              style={{
+                color: "white",
+                fontSize: "20px",
+              }}
+            >
+              Continue
+            </Typography.Text>
           </Button>
-          <Button type="default" style={{ width: "100%" }}>
-            <GoogleOutlined />
-            Log in with Google
+          <div
+            style={{
+              width: "100%",
+              height: "20px",
+              borderBottom: "1px solid black",
+              textAlign: "center",
+              clear: "right",
+              paddingTop: "1rem",
+              marginBottom: "2rem",
+              padding: 0,
+            }}
+          >
+            <span
+              style={{
+                fontSize: "20px",
+                backgroundColor: "#F3F5F6",
+                padding: "0 10px",
+              }}
+            >
+              Or
+            </span>
+          </div>
+          <Button
+            type="default"
+            style={{
+              width: "100%",
+              borderRadius: "50px",
+              padding: "24px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <GoogleIcon />
+            <Typography.Text
+              strong
+              style={{
+                fontSize: "20px",
+                marginLeft: "1rem",
+              }}
+            >
+              Continue with Google
+            </Typography.Text>
           </Button>
         </Form.Item>
       </Form>
