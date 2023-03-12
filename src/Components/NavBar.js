953aac13969b4684955d9913cb3671f8dcb04941
luanvohcmut2911 <luan.vo2003@hcmut.edu.vn>
2023-03-12 12:39:04 +0700

    signin + signup done

---
+++ src/Components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../asset/reCollect.png';
+
+export default function NavBar() {
+  return (
+    <div style={{
+      backgroundColor: 'black'
+    }}>
+      <img src={logo} alt="logo"/>
+    </div>
+  )
+}
